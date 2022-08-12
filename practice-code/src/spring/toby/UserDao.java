3e005a207341cbe1136815e8c6be46a97b8e5306
hahanari <hahanari@hanmail.net>
2022-08-12 17:19:06 +0900

    [2022.08.12.01] 토비의 스프링 > 1. 오브젝트와 의존관계 (~ p70)

--- practice-code/src/spring/toby/UserDao.java
+++ practice-code/src/spring/toby/UserDao.java
@@ -6,11 +6,13 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class UserDao {
+public abstract class UserDao {
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
 
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/local_db", "iris", "iris");
+        // 관심1: connection
+        Connection c = getConnection();
+
+        // 관심2: SQL 문장을 담을 Statement
         PreparedStatement ps = c.prepareStatement("insert into local_db.users(id, name, password) values(?, ?, ?)");
 
         ps.setString(1, user.getId());
@@ -19,13 +21,15 @@ public class UserDao {
 
         ps.executeUpdate();
 
+        // 관심3: 리소스 반납
         ps.close();
         c.close();
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/local_db", "iris", "iris");
+
+        //ㅇ메서드 추출 기법
+        Connection c = getConnection();
         PreparedStatement ps = c.prepareStatement("select * from local_db.users where id = ?");
 
         ps.setString(1, id);
@@ -42,5 +46,23 @@ public class UserDao {
         ps.close();
         c.close();
         return user;
+    }
+
+    // 추상 메서드 (구현은 서브클래스가 담당)
+    // 팩토리 메서드
+    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
+
+    public class NUserDao extends UserDao {
+        public Connection getConnection() throws ClassNotFoundException, SQLException {
+            Class.forName("com.mysql.jdbc.Driver");
+            return DriverManager.getConnection("jdbc:mysql://localhost/local_db", "iris", "iris");
+        }
+    }
+
+    public class DUserDao extends UserDao {
+        public Connection getConnection() throws ClassNotFoundException, SQLException {
+            Class.forName("com.mysql.jdbc.Driver");
+            return DriverManager.getConnection("jdbc:mysql://localhost/local_db", "iris", "iris");
+        }
     }
 }
